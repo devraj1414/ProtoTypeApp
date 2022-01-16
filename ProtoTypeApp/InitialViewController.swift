5cc9a322b87c1cd3b644d2cab54dbd4bf928f7c2
Suruchi Kumari <suruchikumari@Suruchis-MBP.home>
2022-01-16 16:22:11 -0500

    Initial Commit

---
+++ ProtoTypeApp/InitialViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  ProtoTypeApp
+//
+//  Created by Suruchi Kumari on 1/12/22.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+/**
+ 
+        Fastlane to Automate App to upload on App Store
+ 
+ 
+        #Git on iOS Project:
+        Set the latest Command Line Tool from xCode
+        Else the git init will not recognise and ask you to set the command Line tool
+    
+        #Fastlane in iOS project:
+        => bundle init   // Create a gemfile in project directory(Gemfile)
+ 
+ Note: GitHub is improving security by dropping older, insecure key types.
+
+ DSA keys (ssh-dss) are no longer supported. Existing keys will continue to function through March 15, 2022. You cannot add new DSA keys to your user account on GitHub.com.
+
+ RSA keys (ssh-rsa) with a valid_after before November 2, 2021 may continue to use any signature algorithm. RSA keys generated after that date must use a SHA-2 signature algorithm. Some older clients may need to be upgraded in order to use SHA-2 signatures.
+ 
+ 
+ ls rsa key
+    
+ 
+ 
+ #To Reset or Edit the identity for Git Account on Mac machine is
+ =>  git config --global --edit
+ 
+ 
+ */
+
+/*
+2019189573
+https://www.apartmentfinder.com/New-Jersey/Jersey-City-Apartments/225-Academy-St-Apartments-xv6254m
+*/
---
+++ ProtoTypeApp/ListingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ListingViewController.swift
+//  ProtoTypeApp
+//
+//  Created by Suruchi Kumari on 1/12/22.
+//
+
+import UIKit
+
+class ListingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
